11edd682eafad4e364025792b6a90ccb8ce1cd34
iamrajbhattarai <raj.bhattarai222@gmail.com>
2022-01-30 21:05:09 +0545

    LayerToggler

--- utilities/static/utilities/js/map.js
+++ utilities/static/utilities/js/map.js
@@ -18,6 +18,16 @@ var osmLayer = new ol.layer.Tile({
   source: new ol.source.OSM()
 });
 
+var bingMapLayer = new ol.layer.Tile({
+  visible: false,
+  preload: Infinity,
+  source: new ol.source.BingMaps({
+    key: "Al8ndH_tSrjQGgBTDzeRsN4v97ct11yzrS78nH4i5zn960SjjFRC9WlmBLIBtHzc",
+    imagerySet: "Aerial",
+  }),
+  title: "BingMaps",
+});
+
 var buildingsLayer = new ol.layer.Vector({
   source: new ol.source.Vector({
     url: 'http://localhost:8000/buildings/',
@@ -26,13 +36,20 @@ var buildingsLayer = new ol.layer.Vector({
   style: styles['buildings'],
 });
 
+//LayerGroup
+var layerGroup = new ol.layer.Group({
+  layers: [
+    osmLayer,
+    bingMapLayer,
+    buildingsLayer,
+  ],
+});
+
+
 //creating a map element
 var map = new ol.Map({
     target: 'map',
-    layers: [
-      osmLayer,
-      buildingsLayer,
-    ],
+    layers: [layerGroup],
     view: new ol.View({
       center: [85.3222, 27.753014],
       zoom: 18,
@@ -40,3 +57,25 @@ var map = new ol.Map({
       multiWorld: true,
     })
   });
+
+
+
+//logic for functioning of layer selector
+
+function bindInputs(layerid, layer) {
+  const visibilityInput = $(layerid + " input.visible");
+  visibilityInput.on("change", function () {
+    layer.setVisible(this.checked);
+  });
+}
+function setup(id, group) {
+  group.getLayers().forEach(function (layer, i) {
+    const layerid = id + i;
+    bindInputs(layerid, layer);
+    // if (layer instanceof ol.layer.Group) {
+    //   setup(layerid, layer);
+    // }
+  });
+}
+
+setup("#layer", layerGroup);
