a057b5f0cbfcdb59ad8ea780e61cda1a41533780
iamrajbhattarai <raj.bhattarai222@gmail.com>
2022-03-07 23:58:14 +0545

    Base Command for uploading data to models

--- utilities/load.py
+++ utilities/load.py
@@ -5,26 +5,17 @@ from django.contrib.gis.utils import LayerMapping
 from .models import Building
 
 # building_mapping = {
-#     'ownername': 'OwnerName',
-#     'housenumber': 'HouseNumbe',
 #     'block': 'Block',
-#     'shape_length': 'SHAPE_Leng',
-#     'shape_area': 'SHAPE_Area',
+#     'names': 'Names',
+#     'area': 'Area',
 #     'geom': 'MULTIPOLYGON',
 # }
 
-building_mapping = {
-    'block': 'Block',
-    'names': 'Names',
-    'area': 'Area',
-    'geom': 'MULTIPOLYGON',
-}
-
-building_shp = Path(__file__).resolve().parent / \
-    'Shapefiles' / 'Building.shp'
+# building_shp = Path(__file__).resolve().parent / \
+#     'Shapefiles' / 'Building.shp'
 
 
-def run(verbose=True):
-    lm = LayerMapping(Building, building_shp,
-                      building_mapping, transform=False)
-    lm.save(strict=True, verbose=verbose)
+# def run(verbose=True):
+#     lm = LayerMapping(Building, building_shp,
+#                       building_mapping, transform=False)
+#     lm.save(strict=True, verbose=verbose)
